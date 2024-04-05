5b6b1c56425e782b8f0569e3016b63f815b21ceb
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:24:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Desenvolva um sistema para gerenciar uma frota de veículos, com recursos como rastreamento de veículos, manutenção preventiva, agendamento de rotas e otimização de frota.
---
+++ hermes-spring/src/main/java/br/org/gestao/hermes/HermesApplication.java
@@ -0,0 +1,13 @@
+package br.org.gestao.hermes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesApplication.class, args);
+	}
+
+}
